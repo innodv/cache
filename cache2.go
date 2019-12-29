f3b46a7ec7fb51600992b3a1c1ad2d318355d5c1
n8wb <vanish172@gmail.com>
2019-12-29 13:34:32 -0800

    add a second cache which works with json

---
+++ cache2.go
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2019 Innodev LLC. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package cache
+
+import (
+	"encoding/json"
+	
+	"github.com/gregjones/httpcache/diskcache"
+	lru "github.com/hashicorp/golang-lru"
+)
+
+type Cache2 interface {
+	Add(key string, value interface{}) error
+	Get(key string, out interface{})  (ok bool, err error)
+	Remove(key string)
+}
+
+type cacher2 struct {
+	cache  *lru.Cache
+	cache2 *diskcache.Cache
+}
+
+func New2(size int, dir string) Cache2 {
+	out := &cacher2{
+		cache2: diskcache.New(dir),
+	}
+	c, err := lru.New(size)
+	if err != nil {
+		panic(err)
+	}
+	out.cache = c
+	return out
+}
+
+
+func (c *cacher2) Add(key string, value interface{}) error {
+	data, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	c.cache.Add(key, data)
+	c.cache2.Set(key, data)
+	return err
+}
+
+func (c *cacher2) Get(key string, out interface{})  (ok bool, err error) {
+	val, ok := c.cache.Get(key)
+	if ok {
+		err = json.Unmarshal(val.([]byte), out)
+		if err != nil {
+			return false, err
+		}
+		return
+	}
+	data, ok := c.cache2.Get(key)
+	if ok {
+		err = json.Unmarshal(data, out)
+		if err != nil {
+			return false, err
+		}
+		return
+	}
+	return false, nil
+}
+
+func (c *cacher2) Remove(key string) {
+	c.cache.Remove(key)
+	c.cache2.Delete(key)
+}
---
+++ cache2_test.go
@@ -0,0 +1,30 @@
+/**
+ * Copyright 2019 Innodev LLC. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package cache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+
+func TestCache2(t *testing.T) {
+	testVal := map[string]string{
+		"foo":"bar",
+	}
+	c := New2(200, "/tmp/foobar")
+	c.Add("hello",testVal)
+	var val map[string]string
+	ok, err := c.Get("hello", &val)
+	assert.Equal(t, testVal, val)
+	assert.True(t, ok)
+	assert.NoError(t, err)
+	c.Remove("hello")
+	ok, _ = c.Get("hello", &val)
+	assert.False(t, ok)
+}
