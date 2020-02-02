1ff21adb4a4e02c6d062483f156a053b55b08787
n8wb <vanish172@gmail.com>
2020-02-01 19:49:44 -0800

    redis support

---
+++ cache3_test.go
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2019 Innodev LLC. All rights reserved.
+ * Use of this source code is governed by a BSD-style
+ * license that can be found in the LICENSE file.
+ */
+
+package cache
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCacheRedis(t *testing.T) {
+	testVal := map[string]string{
+		"foo": "bar",
+	}
+	c := NewRedis(1, RedisConfig{
+		Addr:     "127.0.0.1:6379",
+		Network:  "tcp",
+		Password: "password",
+		Database: 1,
+	})
+	c.Add("hello", testVal)
+	c.Add("hello2", testVal)
+	c.Add("hello3", testVal)
+	var val map[string]string
+	ok, err := c.Get("hello", &val)
+	assert.Equal(t, testVal, val)
+	assert.True(t, ok)
+	assert.NoError(t, err)
+	c.Remove("hello")
+	ok, _ = c.Get("hello", &val)
+	assert.False(t, ok)
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/innodv/cache
 go 1.13
 
 require (
+	github.com/gomodule/redigo v2.0.0+incompatible
 	github.com/google/btree v1.0.0 // indirect
 	github.com/gregjones/httpcache v0.0.0-20190611155906-901d90724c79
 	github.com/hashicorp/golang-lru v0.5.3
